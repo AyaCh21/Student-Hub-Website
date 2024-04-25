f648d2f18e655697e68ff9298dfee17cd90f18ae
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2024-04-25 11:04:15 +0200

    sketch of the profile page

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,10 +8,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomePageController extends AbstractController
 {
+    private array $stylesheets;
     #[Route("/home", name:"homepage")]
     public function homePage(): Response
     {
         return $this->render('home.html.twig',[]);
+    }
+
+    #[Route("/profile", name:"profile")]
+    public function profilePage(): Response
+    {
+        $this->stylesheets[]='profile.css';
+        return $this->render('profile.html.twig',[
+            'stylesheets'=>$this->stylesheets
+        ]);
     }
 
 }

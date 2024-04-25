def49b743b7b2a8f98becfff7ed7b9a03fc95b7d
Bolin Li <bolin.li@student.kuleuven.be>
2024-04-25 10:22:52 +0200

    installed the Authentication form the composer.json

--- src/Controller/SecureLoginController.php
+++ src/Controller/SecureLoginController.php
@@ -1,16 +1,52 @@
 <?php
 
+
+//===================================================================//
+//                      good links
+//===================================================================//
+//  How to Build a Login Form
+//  https://symfony.com/doc/4.x/security/form_login_setup.html
+//
+//  security doc
+//  https://symfony.com/doc/current/security.html
+//
+///////////////////////////////////////////////////////////////////////
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecureLoginController extends AbstractController
 {
-    #[Route("/login", name:"loginPage")]
-    public function loginPage(): Response
+    #[Route('/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
-        return $this->render('login.html.twig');
+        // Get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Retrieve username and password from the form submission
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+
+        printf("user: %s, password:%s",$username,$password);
+
+        return $this->render('login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/login_check', name: 'login_check')]
+    public function loginCheck(Request $request)
+    {
+        // This method is only used to define the route for login form submission.
+        // Symfony's security system will handle the actual authentication process.
+        throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
     }
 }

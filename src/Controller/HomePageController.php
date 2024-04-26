14578d155838ef264b26e1f0da12702f6532b17c
Aya <aya.chaounibenabdallah@student.kuleuven.be>
2024-04-26 23:04:40 +0200

    fixing branches is difficult

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,10 +8,31 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomePageController extends AbstractController
 {
+    private array $stylesheets;
     #[Route("/home", name:"homepage")]
     public function homePage(): Response
     {
-        return $this->render('home.html.twig');
+        $this->stylesheets[]='home_header.css';
+        return $this->render('home.html.twig',[
+            'stylesheets'=>$this->stylesheets
+        ]);
     }
+
+    #[Route("/profile", name:"profile")]
+    public function profilePage(): Response
+    {
+        $this->stylesheets[]='profile.css';
+        return $this->render('profile.html.twig',[
+            'stylesheets'=>$this->stylesheets
+        ]);
+    }
+    //#[Route("/study", name:"study")]
+    //public function study(): Response
+    //{
+    //  $this->stylesheets[]='study.css';
+    //return $this->render('study.html.twig',[
+    //  'stylesheets'=>$this->stylesheets
+    //]);
+    //}
 
 }

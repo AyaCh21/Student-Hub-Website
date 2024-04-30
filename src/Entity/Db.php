2b1d2dc5c8d9509fbee5904b2f0ad69a9e14dee5
zhiwei.wen <Zhiwei.wen@student.kuleuven.be>
2024-04-30 15:20:55 +0200

    test

--- src/Entity/Db.php
+++ src/Entity/Db.php
@@ -2,7 +2,25 @@
 
 namespace App\Entity;
 
+use PDO;
+
 class Db
 {
+    // constants for database connection
+    const DSN = 'mysql:dbname=@mysql.studev.groept.be;host=mysql.studev.groept.be';
+    const USERNAME = "a23www301";
+    const PASSWORD = "ra3wnmlO";
+    private PDO $connection;
+    private static Db $instance;
 
+    private function __construct()
+    {
+        $this->connection=new PDO(self::DSN,self::USERNAME,self::PASSWORD);
+    }
+    public static function getConnection():PDO{
+        if(!isset(self::$instance)){
+            self::$instance=new Db();
+        }
+        return self::$instance->connection;
+    }
 }

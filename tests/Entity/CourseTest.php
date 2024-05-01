5dbf97cd60aeda595b211dcc6e3c68a9c78c762f
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2024-05-01 15:15:25 +0200

    created the first test class

---
+++ tests/Entity/CourseTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Course;
+use PHPUnit\Framework\TestCase;
+
+class CourseTest extends TestCase
+{
+
+    public function testGetId()
+    {
+
+    }
+
+    public function testGetName()
+    {
+
+    }
+
+    public function testSetId()
+    {
+
+    }
+
+    public function testSetName()
+    {
+
+    }
+
+    public function testGetProfessor()
+    {
+
+    }
+
+    public function testSetProfessor()
+    {
+
+    }
+}

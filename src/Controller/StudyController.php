7ef19c5cb64bed52a72dc553fe12cdbe02c0cb6a
Student <oladelejoshuaagboola.ayeni@student.kuleuven.be>
2024-05-02 11:19:24 +0200

    pdfs working

--- src/Controller/StudyController.php
+++ src/Controller/StudyController.php
@@ -21,31 +21,52 @@ class StudyController extends AbstractController
             [
                 'name' => 'Phase 1',
                 'courses' => [
-                    ['name' => 'Course A1'],
-                    ['name' => 'Course A2'],
+                    ['name' => 'Course A1',
+//                        'pdf' => 'http://a23www301.studev.groept.be/DeleAyeni_Transistor_Lab3_Prep.pdf'
+                    'pdf' => null
+                    ],
+                    ['name' => 'Course A2',
+                        'pdf' => null
+                    ],
                 ],
             ],
             [
                 'name' => 'Phase 2',
                 'courses' => [
-                    ['name' => 'Course B1'],
-                    ['name' => 'Course B2'],
+                    ['name' => 'Course B1',
+                        'pdf' => 'http://a23www301.studev.groept.be/TDI_Lab_1.pdf'
+                        ],
+                    ['name' => 'Course B2',
+                        'pdf' => null
+                    ],
                 ],
             ],
             [
                 'name' => 'Phase 3',
                 'courses' => [
-                    ['name' => 'Course C1'],
-                    ['name' => 'Course C2'],
-                    ['name' => 'Course C3'],
+                    ['name' => 'Course C1',
+                        'pdf' => null
+                    ],
+                    ['name' => 'Course C2',
+                        'pdf' => null
+                    ],
+                    ['name' => 'Course C3',
+                        'pdf' => null
+                    ],
                 ],
             ],
             [
                 'name' => 'Phase 4',
                 'courses' => [
-                    ['name' => 'Course D1'],
-                    ['name' => 'Course D2'],
-                    ['name' => 'Course D3'],
+                    ['name' => 'Course D1',
+                        'pdf' => null
+                    ],
+                    ['name' => 'Course D2',
+                        'pdf' => null
+                    ],
+                    ['name' => 'Course D3',
+                        'pdf' => null
+                    ],
                 ],
             ],
         ];

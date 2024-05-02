042fd2e0a45defbddc01d4618b1d68a15f91dccb
Aya <aya.chaounibenabdallah@student.kuleuven.be>
2024-05-02 11:47:39 +0200

    phpunit not allowing some functions had to remove it and added the professortest and coursetest

--- tests/Entity/CourseTest.php
+++ tests/Entity/CourseTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Course;
+use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
+use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 
 class CourseTest extends TestCase
@@ -10,31 +12,78 @@ class CourseTest extends TestCase
 
     public function testGetId()
     {
-
-    }
-
-    public function testGetName()
-    {
-
+        $course = new Course();
+        $course->setId(1);
+        $this->assertEquals(1, $course->getId());
     }
 
     public function testSetId()
     {
+        $course = new Course();
+        $course->setId(1);
+        $this->assertEquals(1, $course->getId());
+    }
 
+    public function testGetName()
+    {
+        $course = new Course();
+        $course->setName("Mathematical Modelling");
+        $this->assertEquals("Mathematical Modelling", $course->getName());
     }
 
     public function testSetName()
     {
-
+        $course = new Course();
+        $course->setName("Chemistry");
+        $this->assertEquals("Chemistry", $course->getName());
     }
 
     public function testGetProfessor()
     {
-
+        $course = new Course();
+        $course->setProfessor(1);
+        $this->assertEquals(1, $course->getProfessor());
     }
 
     public function testSetProfessor()
     {
+        $course = new Course();
+        $course->setProfessor(6);
+        $this->assertEquals(6, $course->getProfessor());
+    }
 
+    public function testFailSetProfessor()
+    {
+        $course = new Course();
+        $course->setProfessor(6);
+        $this->assertNotEquals(7, $course->getProfessor());
+    }
+    public function testFailSetName()
+    {
+        $course = new Course();
+        $course->setName("Chemistry");
+        $this->assertNotEquals("Biology", $course->getName());
+        $this->assertEquals("Chemistry", $course->getName());
+    }
+    public function testFailSetId()
+    {
+        $course = new Course();
+        $course->setId(1);
+        $this->assertNotEquals(2, $course->getId());
+    }
+    #[group('deprecated')]
+    #[doesNotPerformAssertions]
+    public function testSetIdWithDeprecation()
+    {
+        $course = new Course();
+        $course->setId(1);
+    }
+
+    #[group('deprecated')]
+    #[doesNotPerformAssertions]
+    public function testSetProfessorWithDeprecation()
+    {
+        $course = new Course();
+        $course->setProfessor(6);
     }
 }

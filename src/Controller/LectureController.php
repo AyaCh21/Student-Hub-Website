62b12d30505feb3994e1e324ad740e5ea8ab7d2f
rohan <rohan.bhattaram@student.kuleuven.be>
2024-05-18 22:47:15 +0200

    Now we can see the lecture and lab notes of the course we clicked on in the study page.

--- src/Controller/LectureController.php
+++ src/Controller/LectureController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Course;
 use App\Entity\StudyMaterial;
-use App\Repository\CourseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,30 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LectureController extends AbstractController
 {
-    private array $stylesheets = [];
-
-    /**
-     * @Route("/lecture", name="lecture")
-     */
-    #[Route("/lecture", name:"lecture")]
-    public function lecture(EntityManagerInterface $entityManager): Response
+    #[Route("/lecture/{id}/{type}", name: "lecture")]
+    public function lecture(int $id, string $type, EntityManagerInterface $entityManager): Response
     {
-        $studyMaterials = $entityManager->getRepository(StudyMaterial::class)->findAll();
-        $typeWiseMaterials = [];
-
-        foreach ($studyMaterials as $material) {
-            $type = $material->getType();
-            if (!isset($typeWiseMaterials[$type])) {
-                $typeWiseMaterials[$type] = [];
-            }
-            $typeWiseMaterials[$type][] = $material;
-        }
-
-        $this->stylesheets[] = 'lecture.css';
+        $studyMaterials = $entityManager->getRepository(StudyMaterial::class)->findBy([
+            'course' => $id,
+            'type' => $type
+        ]);
 
         return $this->render('lecture.html.twig', [
-            'stylesheets' => $this->stylesheets,
-            'typeWiseMaterials' => $typeWiseMaterials
+            'studyMaterials' => $studyMaterials,
+            'type' => ucfirst($type)  // Capitalize the first letter of the type for display
         ]);
     }
 }

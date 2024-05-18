91636f73a71ba9dc039dd918bc83dd384a5dc648
zhiwei.wen <Zhiwei.wen@student.kuleuven.be>
2024-05-18 11:53:56 +0200

    favorite ORM

---
+++ src/Entity/Favorite.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FavoriteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+#[ORM\Entity(repositoryClass: FavoriteRepository::class)]
+#[ORM\Table('Favorite')]
+class Favorite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'StudentID', type: 'integer', nullable: true)]
+    #[ORM\ManyToOne(targetEntity: "Student")]
+    #[ORM\JoinColumn(name: "StudentID", referencedColumnName: "id")]
+    private ?int $StudentID = null;
+
+    #[ORM\Column(name: 'CourseID', type: 'integer', nullable: true)]
+    #[ORM\ManyToOne(targetEntity: "course")]
+    #[ORM\JoinColumn(name: "CourseID", referencedColumnName: "id")]
+    private ?int $CourseID = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getStudentID(): ?int
+    {
+        return $this->StudentID;
+    }
+
+    public function setStudentID(?int $StudentID): static
+    {
+        $this->StudentID = $StudentID;
+
+        return $this;
+    }
+
+    public function getCourseID(): ?int
+    {
+        return $this->CourseID;
+    }
+
+    public function setCourseID(?int $CourseID): static
+    {
+        $this->CourseID = $CourseID;
+
+        return $this;
+    }
+}

ceaf4a61f47600ab6aa15f70b8648e4f8f53514c
Bolin Li <bolin.li@student.kuleuven.be>
2024-05-19 19:53:07 +0200

    reset bot, password:nmsl2020

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Entity\Student;
 use App\Form\ChangePasswordFormType;
 use App\Form\ResetPasswordRequestFormType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -89,7 +89,7 @@ class ResetPasswordController extends AbstractController
         }
 
         try {
-            /** @var User $user */
+            /** @var Student $user */
             $user = $this->resetPasswordHelper->validateTokenAndFetchUser($token);
         } catch (ResetPasswordExceptionInterface $e) {
             $this->addFlash('reset_password_error', sprintf(
@@ -131,7 +131,7 @@ class ResetPasswordController extends AbstractController
 
     private function processSendingPasswordResetEmail(string $emailFormData, MailerInterface $mailer): RedirectResponse
     {
-        $user = $this->entityManager->getRepository(User::class)->findOneBy([
+        $user = $this->entityManager->getRepository(Student::class)->findOneBy([
             'email' => $emailFormData,
         ]);
 

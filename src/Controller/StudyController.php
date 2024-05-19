0dee542e4b9a389191b89ebfdbc48e6d19215799
zhiwei.wen <Zhiwei.wen@student.kuleuven.be>
2024-05-19 11:15:02 +0200

    toggle function in StudyController

--- src/Controller/StudyController.php
+++ src/Controller/StudyController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Course;
+use App\Entity\Favorite;
 use App\Repository\CourseRepository;
+use App\Repository\FavoriteRepositoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class StudyController extends AbstractController
 {
@@ -34,6 +38,32 @@ class StudyController extends AbstractController
             'stylesheets' => $this->stylesheets,
             'phaseWiseCourses' => $phaseWiseCourses
         ]);
+    }
+
+    #[Route("/favorite/toggle", name: "favorite_toggle", methods: ["POST"])]
+    public function toggleFavorite(Request $request, EntityManagerInterface $entityManager, FavoriteRepository $favoriteRepository, UserInterface $user): JsonResponse
+    {
+        $courseId = $request->request->get('courseId');
+        $course = $entityManager->getRepository(Course::class)->find($courseId);
+
+        if (!$course) {
+            return new JsonResponse(['status' => 'error', 'message' => 'Course not found'], 404);
+        }
+
+        $favorite = $favoriteRepository->findOneBy(['user' => $user, 'course' => $course]);
+
+        if ($favorite) {
+            $entityManager->remove($favorite);
+            $entityManager->flush();
+            return new JsonResponse(['status' => 'removed']);
+        } else {
+            $favorite = new Favorite();
+            $favorite->setUser($user);
+            $favorite->setCourse($course);
+            $entityManager->persist($favorite);
+            $entityManager->flush();
+            return new JsonResponse(['status' => 'added']);
+        }
     }
 
 

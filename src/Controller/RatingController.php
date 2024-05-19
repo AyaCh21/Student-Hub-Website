c44b92946489c18fe64ada521f9829abfe516a1f
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2024-05-19 17:25:46 +0200

    cleaning up the rating controller

--- src/Controller/RatingController.php
+++ src/Controller/RatingController.php
@@ -1,12 +1,20 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Course;
+use App\Entity\Professor;
+use App\Entity\ProfessorRate;
 use App\Entity\rating_exam;
+use App\Entity\Student;
 use App\Form\RatingType;
 use App\Repository\CourseRepository;
 use App\Repository\RatingExamRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +22,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RatingController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
+    private array $stylesheets;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
@@ -21,9 +30,9 @@ class RatingController extends AbstractController
     }
 
     /**
-     * @Route("/rate", name="rate_course")
+     * @Route("/rate_course", name="course_rate")
      */
-    public function rateCourse(Request $request): Response
+    public function addCourseRate(Request $request): Response
     {
         $rating = new rating_exam(); // Instantiate rating_exam without constructor parameters
 
@@ -61,9 +70,9 @@ class RatingController extends AbstractController
     }
 
     /**
-     * @Route("/ratings", name="display_ratings")
+     * @Route("/display_rate_course", name="display_course_rate")
      */
-    public function displayRatings(RatingExamRepository $ratingRepository, CourseRepository $courseRepository): Response
+    public function viewCourseRate(RatingExamRepository $ratingRepository, CourseRepository $courseRepository): Response
     {
         // Get average ratings for each course
         $averageRatings = $ratingRepository->getAverageRatings();
@@ -78,6 +87,109 @@ class RatingController extends AbstractController
         return $this->render('display_ratings.html.twig', [
             'averageRatings' => $averageRatings,
             'courseNames' => $courseNames,
+        ]);
+    }
+
+    #[\Symfony\Component\Routing\Attribute\Route("/rate_prof", name:"professor_rate")]
+    public function addProfRate(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $professor_rate = new ProfessorRate();
+        $professors = $entityManager->getRepository(Professor::class)->findAll();
+
+        $form = $this->createFormBuilder($professor_rate)
+            ->add('professor',ChoiceType::class,[
+                'choices'=>$professors,
+                'choice_label'=>function (Professor $professor) {
+                    return $professor->getName(); // Assuming getName() exists
+                },
+                'placeholder' => '-- Select Professor --',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Please select a professor.',
+                    ]),
+                ],
+                'label' => 'choose the professor:'
+            ])
+            ->add('student', EntityType::class, [
+                'class' => Student::class,
+                'choice_label' => function (Student $student) {
+                    return $student->getId();
+                },
+            ])
+            ->add('rate',RangeType::class,[
+                'attr' => ['min' => 0, 'max' => 10],
+                'label' => 'choose the rate:'
+            ])
+            ->add('save',SubmitType::class,['label'=>'submit rate'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $studentId = $form->get('student')->getData();
+            $student = $entityManager->getRepository(Student::class)->find($studentId);
+            if (!$student) {
+                echo "Student not found.";
+            } else {
+                $professor_rate->setStudent($student);
+            }
+
+            $rate = $form->get('rate')->getData();
+            $professor_rate->setRate($rate);
+
+            $entityManager->persist($professor_rate);
+            $entityManager->flush();
+            return $this->redirectToRoute('study');
+        }
+
+        $this->stylesheets[]='rate_form.css';
+        $this->scripts[]='rate_range_prof.js';
+
+        return $this->render('rate_professor.html.twig',[
+            'form_rate_prof' => $form->createView(),
+            'stylesheets'=>$this->stylesheets,
+            'scripts'=>$this->scripts
+        ]);
+    }
+
+    #[Route("/display_rate_prof", name:"display_professor_rate")]
+    public function viewProfRate(EntityManagerInterface $entityManager): Response
+    {
+        $courses = $entityManager->getRepository(Course::class)->findAll();
+        $professorWiseCourses = [];
+        $professorRatings = [];
+        $professorVotes = [];
+
+        foreach ($courses as $course) {
+            $professor = $course->getProfessor();
+            $professorName = $professor->getName();
+            if (!isset($professorWiseCourses[$professorName])) {
+                $professorWiseCourses[$professorName] = [];
+            }
+            $professorWiseCourses[$professorName][] = $course;
+        }
+
+        $professors = $entityManager->getRepository(Professor::class)->findAll();
+
+        foreach ($professors as $professor) {
+            $rates = $entityManager->getRepository(ProfessorRate::class)->findBy(['professor' => $professor]);
+            $totalRate = array_reduce($rates, function ($sum, $rate) {
+                return $sum + $rate->getRate();
+            }, 0);
+
+            $averageRate = count($rates) > 0 ? $totalRate / count($rates) : 0;
+            $professorRatings[$professor->getName()] = $averageRate;
+            $professorVotes[$professor->getName()] = count($rates);
+        }
+
+        $this->stylesheets[] = 'rate_form.css';
+        $this->scripts[] = '';
+
+        return $this->render('display_rate_professor.html.twig', [
+            'stylesheets' => $this->stylesheets,
+            'professorWiseCourses' => $professorWiseCourses,
+            'professorRatings' => $professorRatings,
+            'professorVotes' => $professorVotes,
+            'scripts' => $this->scripts
         ]);
     }
 }

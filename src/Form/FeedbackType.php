65a742ab1be02b2c642b220bae64907467e65eaf
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2024-05-20 17:22:02 +0200

    fixed conflicts in routes, rate

---
+++ src/Form/FeedbackType.php
@@ -0,0 +1,36 @@
+<?php
+// src/Form/FeedbackType.php
+namespace App\Form;
+
+use App\Entity\Feedback;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FeedbackType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('studentUsername', TextType::class, [
+                'label' => 'Your Username',
+                'required' => true,
+            ])
+            ->add('feedback', TextareaType::class, [
+                'label' => 'Your Feedback'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Submit Feedback'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Feedback::class,
+        ]);
+    }
+}

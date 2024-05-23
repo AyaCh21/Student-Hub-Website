0529ae4258e69f2bc6c8d309a93798049d39966a
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2024-05-23 15:00:03 +0200

    fixed merge conflicts in composer

--- src/Controller/LectureController.php
+++ src/Controller/LectureController.php
@@ -2,25 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
 use App\Entity\StudyMaterial;
+use App\Entity\Comment;
+use App\Form\CommentForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LectureController extends AbstractController
 {
     #[Route("/lecture/{id}/{type}", name: "lecture")]
-    public function lecture(int $id, string $type, EntityManagerInterface $entityManager): Response
+    public function lecture(int $id, string $type, EntityManagerInterface $entityManager,Request $request): Response
     {
+        // Fetch the course name
+        $course = $entityManager->getRepository(Course::class)->find($id);
+        $courseName = $course ? $course->getName() : 'Unknown Course';
+
         $studyMaterials = $entityManager->getRepository(StudyMaterial::class)->findBy([
             'course' => $id,
             'type' => $type
         ]);
 
+        $comments = $entityManager->getRepository(Comment::class)->findBy([
+            'course_id' => $id,
+            'type' => $type,
+            'parent_id' => null,
+        ]);
+
+        $comment = new Comment();
+        $form = $this->createForm(CommentForm::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setCourseId($id);
+            $comment->setUserId($this->getUser()->getId());
+            $comment->setType($type);
+            $comment->setCreatedAt(new \DateTime());
+            $comment->setUpdatedAt(new \DateTime());
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('lecture', ['id' => $id, 'type' => $type]);
+        }
+
         return $this->render('lecture.html.twig', [
             'studyMaterials' => $studyMaterials,
-            'type' => ucfirst($type)  // Capitalize the first letter of the type for display
+            'type' => ucfirst($type),  // Capitalize the first letter of the type for display
+            'courseName' => $courseName,  // Pass the course name to the template
+            'comments' => $comments,
+            'commentForm' => $form->createView()
         ]);
     }
 }

a77329a864510f1fffff4f279364d3e1b605c5e8
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2024-05-23 15:00:03 +0200

    fixed merge conflicts in composer

---
+++ src/Entity/Comment.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'integer')]
+    private int $course_id;
+
+    #[ORM\Column(type: 'integer')]
+    private int $user_id;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private string $type;
+
+    #[ORM\Column(type: 'text')]
+    private string $comment_text;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $parent_id = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $created_at;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $updated_at;
+
+    // Getters and setters...
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getCourseId(): int
+    {
+        return $this->course_id;
+    }
+
+    public function setCourseId(int $course_id): self
+    {
+        $this->course_id = $course_id;
+
+        return $this;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getCommentText(): string
+    {
+        return $this->comment_text;
+    }
+
+    public function setCommentText(string $comment_text): self
+    {
+        $this->comment_text = $comment_text;
+
+        return $this;
+    }
+
+    public function getParentId(): ?int
+    {
+        return $this->parent_id;
+    }
+
+    public function setParentId(?int $parent_id): self
+    {
+        $this->parent_id = $parent_id;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTime $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTime $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}

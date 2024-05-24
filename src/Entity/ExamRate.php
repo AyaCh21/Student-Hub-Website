7786349382c10ee902d0f45de8d5ee0452935c23
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2024-05-25 00:46:10 +0200

    fixed merge conflict by accepting this branch version

--- src/Entity/ExamRate.php
+++ src/Entity/ExamRate.php
@@ -40,6 +40,16 @@ class ExamRate
         $this->course = $course;
     }
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
     public function getStudent(): ?Student
     {
         return $this->student;

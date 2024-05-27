df334fd92b335846f6e970f13c73e89b52f7af8b
Bolin Li <bolin.li@student.kuleuven.be>
2024-05-27 14:59:55 +0200

    moved redirectingToLogin test to user controller

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\StudentRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\Student;
+use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    protected static function createKernel(array $options = []): \Symfony\Component\HttpKernel\KernelInterface
+    {
+        // Use your AppKernel class name here
+        return new \App\Kernel('test', true);
+    }
+
+    //in this test, examine whether the redirecting is successful while a unauthorized user is trying to access profile page
+    public function testProfileRedirectToLogin()
+    {
+        try {
+            $client = static::createClient();
+
+            $client->request('GET', '/logout');
+
+            $client->request('GET', '/home');
+            $this->assertResponseStatusCodeSame(200);
+            $this->assertSelectorTextContains('.container-title', 'StudHub!');
+
+            $crawler = $client->request('GET', '/profile');
+            $this->assertSame(302, $client->getResponse()->getStatusCode());
+            $this->assertSame('/login', $client->getResponse()->headers->get('Location'));
+            $crawler = $client->followRedirect();
+            $this->assertSelectorTextContains('h1', 'Login');
+        } catch (\Exception $e) {
+            // Handle the exception gracefully, for example:
+            $this->fail('Exception caught during test: ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+        }
+    }
+
+    public function testProfileRedirectToProfile()
+    {
+        try {
+            $client = static::createClient();
+
+            $userRepository = static::getContainer()->get(StudentRepository::class);
+
+            // retrieve the test user
+            $testUser = $userRepository->findOneBy(['username'=>'dumb']);
+
+            // simulate $testUser being logged in
+            $client->loginUser($testUser);
+
+            $crawler = $client->request('GET', '/profile');
+            $this->assertResponseIsSuccessful();
+
+            $this->assertSame(200, $client->getResponse()->getStatusCode());
+            $this->assertSelectorTextContains('h3', 'Your Profile');
+        } catch (\Exception $e) {
+            // Handle the exception gracefully, for example:
+            $this->fail('Exception caught during test: ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+        }
+    }
+
+}

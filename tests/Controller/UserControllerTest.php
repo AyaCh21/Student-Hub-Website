0499e3a992ee23a832724d568f351433d59e398b
Bolin Li <bolin.li@student.kuleuven.be>
2024-05-28 19:43:05 +0200

    testNonExistUserLogin pass

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -101,6 +101,42 @@ class UserControllerTest extends WebTestCase
         }
     }
 
+    public function testNonExistUserLogin()
+    {
+        // PHPUnit 11 checks for any leftovers in error handlers, manual cleanup
+        $prevHandler = set_exception_handler(null);
+
+        try {
+            $client = static::createClient();
+            $client->followRedirects();
+            $crawler =$client->request('GET', '/login');
+
+            $form = $crawler->selectButton('Login')->form();
+            $form['_username'] = "no";
+            $form['_password'] = "no";
+            $form['_remember_me']->tick();
+            $client->submit($form);
+//            $crawler = $client->submitForm('Login', [
+//                'username' => 'dumb',
+//                'password' => 'dumb',
+//                'remember_me' => false,
+//            ]);
+            $this->assertSame(200, $client->getResponse()->getStatusCode());
+            $this->assertSelectorTextContains('h1', 'Login Page');
+
+            $crawler = $client->request('GET', '/home');
+            $this->assertResponseStatusCodeSame(200);
+            $this->assertSelectorTextContains('.container-title', 'StudHub!');
+            $this->assertCount(1, $crawler->filter('a[href="/register"] input[type="button"][value="Join Now"]'));
+        } catch (\Exception $e) {
+            // Handle the exception gracefully, for example:
+            $this->fail('Exception caught during test: ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+        } finally {
+            // Restore the previous exception handler
+            set_exception_handler($prevHandler);
+        }
+    }
+
 
     //in this test, examine whether the redirecting is successful while a unauthorized user is trying to access profile page
     public function testUnauthenticatedProfileRedirect()

1f7e097a3bf71219ffcb3b2f320fbdc22e5e84bb
Bolin Li <bolin.li@student.kuleuven.be>
2024-05-28 20:22:18 +0200

    testRegisterPageRedirectSuccessful pass

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -137,6 +137,31 @@ class UserControllerTest extends WebTestCase
         }
     }
 
+    public function testRegisterPageRedirectSuccessful()
+    {
+        // PHPUnit 11 checks for any leftovers in error handlers, manual cleanup
+        $prevHandler = set_exception_handler(null);
+
+        try {
+            $client = static::createClient();
+//            $client->followRedirects();
+
+            // Request the login page
+            $crawler = $client->request('GET', '/register');
+
+            $this->assertResponseIsSuccessful();
+            $this->assertResponseStatusCodeSame(200);
+            $this->assertSelectorTextContains('.container-title', 'Register');
+
+        } catch (\Exception $e) {
+            // Handle the exception gracefully, for example:
+            $this->fail('Exception caught during test: ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+        } finally {
+            // Restore the previous exception handler
+            set_exception_handler($prevHandler);
+        }
+    }
+
 
     //in this test, examine whether the redirecting is successful while a unauthorized user is trying to access profile page
     public function testUnauthenticatedProfileRedirect()

36ca2e82558c151baa7dc9f9b72aa0e3ae9e3e16
Bolin Li <bolin.li@student.kuleuven.be>
2024-05-30 01:09:37 +0200

    failed to submit form

--- tests/Controller/FeedbackControllerTest.php
+++ tests/Controller/FeedbackControllerTest.php
@@ -12,7 +12,7 @@ use PHPUnit\Framework\TestCase;
 //======================================================================================================//
 //Note:
 //      this controller directs user into specific feedback page.
-//      hence simple test of redirection and rendering added.
+//      hence simple test of form submit, redirection and rendering added.
 //=====================================================================================================//
 class FeedbackControllerTest extends WebTestCase
 {
@@ -42,6 +42,43 @@ class FeedbackControllerTest extends WebTestCase
             set_exception_handler($prevHandler);
         }
     }
+
+
+    /**
+     * failed to submit form
+     */
+//    public function testFeedbackSubmit()
+//    {
+//        // PHPUnit 11 checks for any leftovers in error handlers, manual cleanup
+//        $prevHandler = set_exception_handler(null);
+//
+//        try {
+//            $client = static::createClient();
+//
+//            //login user
+//            $userRepository = static::getContainer()->get(StudentRepository::class);
+//            $testUser = $userRepository->findOneBy(['username' => 'dumb']);
+//            $client->loginUser($testUser);
+//
+//            $crawler = $client->request('GET', '/feedbacks/1');
+//            $this->assertResponseIsSuccessful();
+//            $this->assertSame(200, $client->getResponse()->getStatusCode());
+//
+//            $form = $crawler->selectButton('Submit Feedback')->form([
+//                'form_course[studentUsername]' => 'dumb',
+//                'form_course[feedback]' => 'dumb feedback',
+//            ]);
+//            $client->submit($form);
+//            $this->assertSame(200, $client->getResponse()->getStatusCode());
+//
+//        } catch (\Exception $e) {
+//            // Handle the exception gracefully, for example:
+//            $this->fail('Exception caught during test: ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+//        } finally {
+//            // Restore the previous exception handler
+//            set_exception_handler($prevHandler);
+//        }
+//    }
 
     public function testViewFeedback()
     {

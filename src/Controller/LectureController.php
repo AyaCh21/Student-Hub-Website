d9f3b06c589f0552de986e979b5acabcc127d1dd
Rohan Bhattaram <rohan.bhattaram@student.kuleuven.be>
2024-05-30 17:23:29 +0000

    Resolve "Fix reply to comments in lecture page"

--- src/Controller/LectureController.php
+++ src/Controller/LectureController.php
@@ -25,8 +25,6 @@ use Psr\Log\LoggerInterface;
 use App\Service\PdfTextExtractor;
 use App\Service\TextSummarizer;
 use App\Service\SummaryPdfGenerator;
-use App\Repository\LabRateRepository;
-use App\Repository\LabInstructorRateRepository;
 
 class LectureController extends AbstractController
 {
@@ -37,8 +35,7 @@ class LectureController extends AbstractController
     }
 
     #[Route("/lecture/{id}/{type}", name: "lecture")]
-    public function lecture(int $id, string $type, EntityManagerInterface $entityManager,Request $request, RatingExamRepository $ratingExamRepository, ProfessorRateRepository $professorRateRepository,
-                            ProfessorRepository $professorRepository, LabRateRepository $labRateRepository, LabInstructorRateRepository $labInstructorRateRepository): Response
+    public function lecture(int $id, string $type, EntityManagerInterface $entityManager, Request $request, RatingExamRepository $ratingExamRepository, ProfessorRateRepository $professorRateRepository, ProfessorRepository $professorRepository): Response
     {
         // Fetch the course name
         $course = $entityManager->getRepository(Course::class)->find($id);
@@ -63,6 +60,7 @@ class LectureController extends AbstractController
         $this->stylesheets[] = 'lecture.css';
         $this->javascripts[] = 'lecture.js';
 
+        // Comment form handling
         $comment = new Comment();
         $commentForm = $this->createForm(CommentForm::class, $comment);
         $commentForm->handleRequest($request);
@@ -89,8 +87,9 @@ class LectureController extends AbstractController
             return $this->redirectToRoute('lecture', ['id' => $id, 'type' => $type]);
         }
 
+        // Reply form handling
         $replyComment = new Comment();
-        $replyForm = $this->createForm(ReplyForm::class, $comment);
+        $replyForm = $this->createForm(ReplyForm::class, $replyComment);
         $replyForm->handleRequest($request);
 
         if ($replyForm->isSubmitted() && $replyForm->isValid()) {
@@ -139,35 +138,13 @@ class LectureController extends AbstractController
                 'type' => $type
             ]);
         }
-        // Fetch average course rating (this is actual an exam rating)
+
         $averageCourseRating = $ratingExamRepository->getAverageRatingForCourse($id);
-
-
-
-        // Fetch lab instructors and their average ratings
-        $labInstructors = $course->getLabInstructors();  // This returns a collection of LabInstructor
-        $labInstructorRatings = [];
-        $labInstructorNames = [];  // Initialize an array to store lab instructor names
-
-        foreach ($labInstructors as $labInstructor) {
-            $labInstructorRatings[] = [
-                'labInstructor' => $labInstructor,
-                'averageRating' => $labInstructorRateRepository->getAverageRatingForLabInstructor($labInstructor->getId())
-            ];
-            $labInstructorNames[] = $labInstructor->getName();  // Add the name to the array
-        }
-
-
-        $averageLabRating = $labRateRepository->getAverageRatingForLab($id);
-
-        // Fetch average professor rating
-        //$averageProfessorRating = $professor ? $professorRateRepository->getAverageRatingForProfessor($professor->getId()) : null;
-
 
         return $this->render('lecture.html.twig', [
             'studyMaterials' => $studyMaterials,
-            'type' => ucfirst($type),  // Capitalize the first letter of the type for display
-            'courseName' => $courseName,  // Pass the course name to the template
+            'type' => ucfirst($type),
+            'courseName' => $courseName,
             'course' => $course,
             'comments' => $comments,
             'commentForm' => $commentForm->createView(),
@@ -178,14 +155,10 @@ class LectureController extends AbstractController
             'averageProfessorRating' => $averageProfessorRating,
             'professorName' => $professorName,
             'professor' => $professor,
-            'form' => $form->createView(),
-            'averageLabRating' => $averageLabRating,
-            'labInstructorName' => $labInstructorNames,  // Pass the array of names to the view
-            'labInstructor' => $labInstructors,  // Pass the collection of lab instructors
-            'labInstructorRatings' => $labInstructorRatings,  // Pass this variable to the view
-
+            'form' => $form->createView()
         ]);
     }
+
     #[Route('/generate_summary', name: 'generate_summary')]
     public function generateSummary(Request $request, EntityManagerInterface $entityManager, PdfTextExtractor $pdfTextExtractor, TextSummarizer $textSummarizer, SummaryPdfGenerator $summaryPdfGenerator): Response
     {
@@ -224,6 +197,7 @@ class LectureController extends AbstractController
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'summary.pdf');
         return $response;
     }
+
     #[Route('/study_material/{id}/view_pdf', name: 'view_pdf')]
     public function viewPdf(int $id, EntityManagerInterface $entityManager): Response {
         $studyMaterial = $entityManager->getRepository(StudyMaterial::class)->find($id);
